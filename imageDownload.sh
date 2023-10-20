9b582bcc322975b69d7aecf0c751270798d6d123
melikkutluay <melikktly@gmail.com>
2023-10-20 03:04:19 +0300

    ...

---
+++ imageDownload.sh
@@ -0,0 +1,4 @@
+docker pull melikktly/rabbitmq:3.12.6-debian-11-r14
+docker pull melikktly/vault:1.3.0
+docker pull melikktly/producer:dep-5
+docker pull melikktly/consumer:dep-5
---
+++ minikubeImageLoad.sh
@@ -0,0 +1,4 @@
+minikube image load melikktly/rabbitmq:3.12.6-debian-11-r14
+minikube image load melikktly/vault:1.3.0
+minikube image load melikktly/producer:dep-5
+minikube image load melikktly/consumer:dep-5
