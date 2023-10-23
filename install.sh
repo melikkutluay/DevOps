1a6d86f2915a8213b21158dc8af502b38e83e4b0
melikkutluay <melikktly@gmail.com>
2023-10-23 14:20:16 +0300

    kubectl fix

--- install.sh
+++ install.sh
@@ -69,9 +69,8 @@ installMicrok8sFunction() {
 installKubectlFunction() {
     echo "install kubectl"
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    chmod +x ./kubectl
-    mkdir -p ~/.local/bin
-    mv ./kubectl ~/.local/bin/kubectl
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin
     kubectl version --client
 }
 
